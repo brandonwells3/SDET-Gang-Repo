c68cd4044801210d3703c80a7303f672132bb330
sbollman726 <samueljbollman@gmail.com>
2023-05-07 19:48:23 -0400

    Adding last of class materials for project

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -1,28 +1,63 @@
 package utilities;
 
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
+import io.github.bonigarcia.wdm.managers.EdgeDriverManager;
+import io.github.bonigarcia.wdm.managers.FirefoxDriverManager;
 
 public class Driver {
 	
-	public static WebDriver driver;
 	
+	private static WebDriver driver;
 	public static WebDriver getDriver() {
-		
-		System.setProperty("webdriver.chrome.driver", "/Users/irina/Desktop/SeleniumTools/chromeDriver/chromedriver_mac_arm64/chromedriver");
-		
-		if(driver == null) {
-			driver = new ChromeDriver();
+		String browser = System.getProperty("browser");
+		if (browser == null) {
+			browser = DataReader.getProperty("browser");
+		}
+		if (driver == null || ((RemoteWebDriver) driver).getSessionId() == null) {
+			switch (browser) {
+			case "firefox":
+				FirefoxDriverManager.firefoxdriver().setup();
+				FirefoxOptions ffoptions = new FirefoxOptions();
+				ffoptions.setHeadless(true);
+				driver = new FirefoxDriver(ffoptions);
+				break;
+			case "edge":
+				EdgeDriverManager.edgedriver().setup();
+				driver = new EdgeDriver();
+				break;
+			case "safari":
+				driver = new SafariDriver();
+				break;
+			case "chrome":
+				ChromeDriverManager.chromedriver().setup();
+				driver = new ChromeDriver();
+				break;
+			case "headless":
+			default:
+				ChromeDriverManager.chromedriver().setup();
+				ChromeOptions options = new ChromeOptions();
+				options.addArguments("--headless");
+				options.addArguments("--window-size=1920,1080");
+				driver = new ChromeDriver(options);
 			}
-			return driver;	
+		}
+		return driver;
 	}
+	
 	public static void quitDriver() {
-		
-		if(driver != null) {
+		if (driver != null) {
 			driver.quit();
 			driver = null;
 		}
-	
 	}
+
 }
