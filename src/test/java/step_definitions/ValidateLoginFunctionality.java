8b11d0d70766c2fc784a5f667d692726aace4ccb
Ira1991Anton <antony.ira1991@gmail.com>
2023-05-09 16:34:00 -0500

    CI-1 code, update

--- src/test/java/step_definitions/ValidateLoginFunctionality.java
+++ src/test/java/step_definitions/ValidateLoginFunctionality.java
@@ -27,9 +27,11 @@ public class ValidateLoginFunctionality {
 
 	@When("User enters email {string} and password {string}")
 	public void user_enters_email_and_password(String email, String password) {
+		loginPage =new CraterLoginPage();
+		utils = new BrowserUtils();
 		emailAddress = email;
 		passwordValue = password;
-		utils.actionsSendKeys(loginPage.craterLoginEmailField, email);
+		utils.actionsSendKeys(loginPage.craterLoginEmailField,email);
 		utils.actionsSendKeys(loginPage.craterLoginPassField, password);
 
 	}
@@ -46,8 +48,6 @@ public class ValidateLoginFunctionality {
 		Assert.assertTrue(loginPage.accountSettingsHeader.isDisplayed());
 
 	}
-	
-	
 
 	// Invalid Login
 	@When("User enters invalid {string} and invalid {string}")
@@ -59,40 +59,65 @@ public class ValidateLoginFunctionality {
 
 	}
 
-	@Then("User should see an error message {string}")
+	@Then("User should see an error message {string} with a close button on the right")
 	public void user_should_see_an_error_message(String errorMessage) {
 		utils.waitUntilElementVisible(loginPage.invalidLoginErrorMessage);
 		Assert.assertTrue(loginPage.invalidLoginErrorMessage.isDisplayed());
-
 	}
 
-//	@Then("The flash message has a close button on the right and disapear in less than or equal to {int} seconds")
-//	public void the_flash_message_has_a_close_button_on_the_right_and_disapear_in_less_than_or_equal_to_seconds(
-//			Integer errorMessage) {
-//
-//	}
+	@Then("I click the X next on the right side of error message")
+	public void i_click_the_x_next_on_the_right_side_of_error_message() {
+		utils.waitUntilElementToBeClickable(loginPage.invalidLoginErrorMessageXBtn);
+		utils.actionsClick(loginPage.invalidLoginErrorMessageXBtn);
+	}
+
+	@Then("The flash message should disapear in less than or equal to five seconds")
+	public void the_flash_message_should_disapear_in_less_than_or_equal_to_five_seconds() throws InterruptedException {
+		try {
+			try {
+				try {
+					try {
+						Assert.assertFalse("Error", loginPage.invalidLoginErrorMessageXBtn.isDisplayed());
+					} catch (java.lang.AssertionError e) {
+						Thread.sleep(3000);
+						Assert.assertFalse("Error", loginPage.invalidLoginErrorMessageXBtn.isDisplayed());
+						System.out.println("3 sec");
+					}
+				} catch (java.lang.AssertionError e) {
+					Thread.sleep(1000);
+					Assert.assertFalse("Error", loginPage.invalidLoginErrorMessageXBtn.isDisplayed());
+					System.out.println("4 sec");
+				}
+			} catch (java.lang.AssertionError e) {
+				Thread.sleep(1000);
+				Assert.assertFalse("Error", loginPage.invalidLoginErrorMessageXBtn.isDisplayed());
+				System.out.println("5 sec");
+			}
+		} catch (org.openqa.selenium.NoSuchElementException e) {
+			System.out.println("Element no longer present");
+		}
+
+	}
 
 	@Then("User should not be logged in")
 	public void user_should_not_be_logged_in() {
 		Assert.assertTrue(loginPage.craterLoginPassField.isDisplayed());
 	}
 
-	
-	
 	// Invalid login, leaving empty email and password field
 	@When("User leaves empty email {string} and password {string} field")
 	public void user_leaves_empty_email_and_password_field(String email, String password) {
+		utils = new BrowserUtils();
+		loginPage = new CraterLoginPage();
 		utils.actionsSendKeys(loginPage.craterLoginEmailField, email);
 		utils.actionsSendKeys(loginPage.craterLoginPassField, password);
-		
-		}
+
+	}
+
 	@Then("User should see the following {string} and  {string}")
 	public void user_should_see_the_following_and(String string, String string2) {
 		Assert.assertTrue(loginPage.fieldIsRequiredMessage.isDisplayed());
-		
-		
-		
-	   
+
 	}
 
 }
